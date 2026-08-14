842d8f4bb97a187e4600289f4e1705aa98976e64
BrianKoge <brnkoge@gmail.com>
2026-08-14 20:43:31 +0300

    Prepare app for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://todo-fullstackibo.netlify.app"
+}));
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
